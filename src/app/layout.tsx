56548a95b48c3f1661c21b99c7c677abc0ce0a9b
Hokuto Marui <hokuto-marui@ngfhd.jp>
2025-12-19 15:08:38 +0900

    エラー修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="ja">
+        <html lang="ja" suppressHydrationWarning={true}>
             <body className={inter.className}>
                 <AppLayout>
                     <AuthProvider>
