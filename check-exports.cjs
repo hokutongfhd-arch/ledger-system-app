0fd9c593ed4e6c05caf6e22c9dd3061bac0f89d4
Hokuto Marui <hokuto-marui@ngfhd.jp>
2026-01-06 15:00:12 +0900

    不要ファイル削除

--- check-exports.cjs
+++
@@ -1,4 +0,0 @@
-const pkg = require('@supabase/auth-helpers-nextjs');
-console.log('--- EXPORTS START ---');
-Object.keys(pkg).forEach(key => console.log(key));
-console.log('--- EXPORTS END ---');
--- src/db/check_audit_reports_schema.sql
+++
@@ -1,5 +0,0 @@
--- check_audit_reports_schema.sql
-SELECT column_name, data_type, is_nullable
-FROM information_schema.columns 
-WHERE table_name = 'audit_reports'
-ORDER BY ordinal_position;
